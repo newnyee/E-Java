29ec7fa7378a65d2d196f00f9bac14982c0c4665
newnyee <121937711+newnyee@users.noreply.github.com>
2023-01-05 17:13:27 +0900

    Add files via upload

---
+++ ch08/sec02/RemoteControl.java
@@ -0,0 +1,5 @@
+package e_java.ch08.sec02;
+
+public interface RemoteControl {
+    public void turnOn();
+}
---
+++ ch08/sec03/RemoteControl.java
@@ -0,0 +1,6 @@
+package e_java.ch08.sec03;
+
+public interface RemoteControl {
+    int MAX_VOLUME = 10;
+    int MIN_VOLUME = 0;
+}
